08c81279c4106b388d7d50aeadc33814cf18f688
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-04-11 12:07:58 +0200

    change styling

--- lib/f_home/widgets/home.dart
+++ lib/f_home/widgets/home.dart
@@ -2,30 +2,55 @@ import 'package:cosmos_gov_web/f_home/services/auth_provider.dart';
 import 'package:cosmos_gov_web/f_subscription/widgets/subscription_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
+const defaultTextStyle = TextStyle(color: Colors.black);
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cosmos Governance',
-      home: Column(
-        children: [
-          Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              final state = ref.watch(authStatusProvider);
-              return state.when(
-                loading: () => const CircularProgressIndicator(),
-                error: (err) => Text("error: " + err.toString()),
-                unauthorized: () => Text("unauthorized"),
-                authorized: () => SubscriptionPage(),
+    return ScreenUtilInit(
+        designSize: const Size(800, 1000),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: () {
+          return MaterialApp(
+            title: 'Cosmos Governance',
+            theme: ThemeData(
+                fontFamily: "Montserrat",
+                primaryTextTheme: TextTheme(
+                  headline1: defaultTextStyle.copyWith(fontSize: 48.sp),
+                  headline2: defaultTextStyle.copyWith(fontSize: 32.sp),
+                  headline3: defaultTextStyle.copyWith(fontSize: 24.sp),
+                )),
+            builder: (context, widget) {
+              ScreenUtil.setContext(context);
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: widget!,
               );
             },
-          ),
-        ],
-      ),
-    );
+            home: Scaffold(
+              body: Column(
+                children: [
+                  Consumer(
+                    builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                      final state = ref.watch(authStatusProvider);
+                      return state.when(
+                        loading: () => const CircularProgressIndicator(),
+                        error: (err) => Text("error: " + err.toString()),
+                        unauthorized: () => Text("unauthorized"),
+                        authorized: () => SubscriptionPage(),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/f_subscription/widgets/subscription_widget.dart
+++ lib/f_subscription/widgets/subscription_widget.dart
@@ -1,45 +1,88 @@
+import 'dart:math';
+
 import 'package:cosmos_gov_web/api/protobuf/dart/subscription_service.pb.dart';
 import 'package:cosmos_gov_web/f_subscription/services/subscription_provider.dart';
+import 'package:cosmos_gov_web/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SubscriptionPage extends StatelessWidget {
   const SubscriptionPage({Key? key}) : super(key: key);
 
+  int getCrossAxisCount(BuildContext context) {
+    if (MediaQuery.of(context).size.width <= 800) {
+      return 2;
+    } else if (MediaQuery.of(context).size.width <= 1000) {
+      return 3;
+    } else {
+      return 4;
+    }
+  }
+
   Widget subscriptionWidget(BuildContext context, List<Subscription> subscriptions) {
     return SizedBox(
-      // width: 500,
-      height: 800,
-      child: GridView.count(
-        crossAxisCount: 5,
-        children: List.generate(
-          subscriptions.length,
-          (index) {
-            return Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              final state = ref.watch(subscriptionStateProvider(subscriptions[index]));
-              return state.when(
-                loaded: (subscription) => Container(
-                  color: subscription.isSubscribed ? Colors.green : Colors.amber,
-                  child: Column(
+      height: 600,
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: getCrossAxisCount(context),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          mainAxisExtent: 50,
+        ),
+        itemCount: subscriptions.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final state = ref.watch(subscriptionStateProvider(subscriptions[index]));
+            const double sidePadding = 12;
+            return state.when(
+              loaded: (subscription) => Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.5,
+                      color: subscription.isSubscribed ? Colors.green : grey,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: InkWell(
+                  onTap: () {
+                    ref.read(subscriptionStateProvider(subscriptions[index]).notifier).toggleSubscription();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Center(
+                      Padding(
+                        padding: const EdgeInsets.only(left: sidePadding),
                         child: Text(
                           subscription.displayName,
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(fontSize: 20.sp),
                         ),
                       ),
-                      ElevatedButton(
-                          onPressed: () {
-                            ref.read(subscriptionStateProvider(subscriptions[index]).notifier).toggleSubscription();
-                          },
-                          child: Text("click"))
+                      subscription.isSubscribed
+                          ? const Padding(
+                              padding: EdgeInsets.only(right: sidePadding),
+                              child: Icon(Icons.check_circle_rounded, color: Colors.green, size: 24),
+                            )
+                          : const SizedBox(width: 24),
                     ],
                   ),
                 ),
-                error: (err) => Text("error: " + err.toString()),
-              );
-            });
-          },
+              ),
+              error: (err) => Text("error: " + err.toString()),
+            );
+          });
+        },
+      ),
+    );
+  }
+
+  Widget searchWidget(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: const TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(),
+          hintText: "Search",
         ),
       ),
     );
@@ -47,24 +90,30 @@ class SubscriptionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Subscriptions"),
-        SizedBox(
-          height: 100,
-          child: Text("Search"),
-        ),
-        Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) {
-            final state = ref.watch(subscriptionListStateProvider);
-            return state.when(
-              loading: () => const CircularProgressIndicator(),
-              loaded: (subscriptions) => subscriptionWidget(context, subscriptions),
-              error: (err) => Text("error: " + err.toString()),
-            );
-          },
-        ),
-      ],
+    double margin = max((MediaQuery.of(context).size.width - 1200) / 2, 0);
+    return Container(
+      padding: const EdgeInsets.all(40),
+      margin: EdgeInsets.symmetric(horizontal: margin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Subscriptions", style: Theme.of(context).primaryTextTheme.headline2),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: searchWidget(context),
+          ),
+          Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              final state = ref.watch(subscriptionListStateProvider);
+              return state.when(
+                loading: () => const CircularProgressIndicator(),
+                loaded: (subscriptions) => subscriptionWidget(context, subscriptions),
+                error: (err) => Text("error: " + err.toString()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
