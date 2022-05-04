bc02d72f0cee94b3c922d89fc1ed5ca8300d1461
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-05-04 16:28:10 +0200

    add web folder

---
+++ build/web/app.js
@@ -0,0 +1,150 @@
+async function getAccountJs(chainId) {
+    await window.keplr.enable(chainId);
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    return accounts[0].address;
+}
+
+function buildGrant(granter, grantee, expiration, denom) {
+    return [{
+        "typeUrl": "/cosmos.authz.v1beta1.MsgGrant",
+        "value": {
+          "granter": granter,
+          "grantee": grantee,
+          "grant": {
+            "authorization": {
+              "typeUrl": "/cosmos.authz.v1beta1.GenericAuthorization",
+              "value": "/cosmos.gov.v1beta1.MsgVote",
+            },
+            "expiration": {"seconds": expiration},
+          },
+        }
+      },
+       {
+         "typeUrl": "/cosmos.feegrant.v1beta1.MsgGrantAllowance",
+         "value": {
+           "granter": granter,
+           "grantee": grantee,
+           "allowance": {
+             "typeUrl": "/cosmos.feegrant.v1beta1.AllowedMsgAllowance",
+             "value": {
+               "allowance": {
+                 "typeUrl": "/cosmos.feegrant.v1beta1.BasicAllowance",
+                 "value": {
+                   "spendLimit": [
+                     {"denom": denom, "amount": "5000"}
+                   ],
+                   "expiration": {"seconds": expiration}
+                 }
+               },
+               "allowedMessages": ["/cosmos.gov.v1beta1.MsgVote"]
+             },
+           },
+         }
+       },
+    ];
+}
+
+function buildRevoke(granter, grantee) {
+    return [
+      {
+        "typeUrl": "/cosmos.authz.v1beta1.MsgRevoke",
+        "value": {
+          "granter": granter,
+          "grantee": grantee,
+          "msgTypeUrl": "/cosmos.gov.v1beta1.MsgVote",
+        },
+      },
+       {
+         "typeUrl": "/cosmos.feegrant.v1beta1.MsgRevokeAllowance",
+         "value": {
+           "granter": granter,
+           "grantee": grantee,
+         },
+       }
+    ];
+}
+
+function buildFee() {
+    return {
+      "amount": [
+        {
+          "denom": 'uosmo',
+          "amount": '5000',
+        },
+      ],
+      "gas": '200000',
+    };
+}
+
+async function grantMsgVoteJs(chainId, rpcAddress, granter, grantee, expiration, denom, debug) {
+    await window.keplr.enable(chainId);
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+
+    const client = await window.SigningStargateClient.connectWithSigner(rpcAddress, offlineSigner);
+
+    client.registry.register("/cosmos.authz.v1beta1.MsgGrant", window.MsgGrant)
+    client.registry.register("/cosmos.feegrant.v1beta1.MsgGrantAllowance", window.MsgGrantAllowance)
+
+    messages = buildGrant(granter, grantee, expiration, denom);
+    fee = buildFee();
+
+    if (debug) console.log(messages);
+
+    const genericAuthorization = messages[0].value.grant.authorization.value;
+    messages[0].value.grant.authorization.value = window.GenericAuthorization.encode(
+        window.GenericAuthorization.fromPartial({msg: genericAuthorization})
+    ).finish();
+
+    const basicAllowanceValue = messages[1].value.allowance.value.allowance.value;
+    const basicAllowance = window.BasicAllowance.fromPartial(basicAllowanceValue);
+    const basicAllowanceEncoded = BasicAllowance.encode(basicAllowance).finish()
+
+    const allowedMessageAllowanceValue = messages[1].value.allowance.value
+    allowedMessageAllowanceValue.allowance.value = basicAllowanceEncoded
+    const allowedMessageAllowanceEncoded = AllowedMsgAllowance.encode(allowedMessageAllowanceValue).finish();
+    messages[1].value.allowance.value = allowedMessageAllowanceEncoded
+
+    try {
+        const result = await client.signAndBroadcast(accounts[0].address, messages, fee, "")
+        if (debug) console.log(result);
+        return result
+    } catch(err) {
+        if (debug) console.log(err);
+        return
+    }
+}
+
+async function revokeMsgVoteJs(chainId, rpcAddress, granter, grantee, debug) {
+    await window.keplr.enable(chainId);
+    const offlineSigner = window.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+
+    const client = await window.SigningStargateClient.connectWithSigner(rpcAddress, offlineSigner);
+
+    client.registry.register("/cosmos.authz.v1beta1.MsgRevoke", window.MsgRevoke)
+    client.registry.register("/cosmos.feegrant.v1beta1.MsgRevokeAllowance", window.MsgRevokeAllowance)
+
+    messages = buildRevoke(granter, grantee);
+    fee = buildFee();
+
+    if (debug) console.log(messages);
+
+    try {
+        const result = await client.signAndBroadcast(accounts[0].address, messages, fee, "")
+        if (debug) console.log(result);
+        return result
+    } catch(err) {
+        if (debug) console.log(err);
+        return
+    }
+}
+
+window.addEventListener("keplr_keystorechange", () => {
+    console.log(window.keplr_keystorechange_dart())
+})
+
+window.logger = (flutter_value) => {
+   console.log({ js_context: this, flutter_value });
+}
